6e335cc808e3c34947b0a4291af3a75c67cf20f8
zicomentor <zicofauzi@gmail.com>
2023-04-05 14:10:44 +0700

    1st upload

---
+++ Assets/Resources/movescene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class movescene : MonoBehaviour {
+
+        public void ChangeMenuScene(string sceneName)
+        {
+            Application.LoadLevel (sceneName);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("QUIT");
+            Application.Quit();
+        }
+}
+
